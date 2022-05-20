510aa01abd3cd2939653d3c13ea717a5c7c83bd4
Connor Morgan <theconnorm@gmail.com>
2022-05-20 11:10:39 -0500

    (Goals controller setup)

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,7 +1,44 @@
-const getGoals = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+// @desc    Get Goals
+//@route    GET /api/goals 
+//@acces    Private
+
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get Goals'})
-}
+})
+
+// @desc    set Goal
+//@route    POST /api/goals 
+//@acces    Private
+
+const setGoal = asyncHandler(async (req, res) => {
+    if(!req.body.text) {
+        res.status(400)
+        throw new Error('Please add text fool')
+    }
+
+    res.status(200).json({message: 'Get Goals'})
+})
+
+// @desc    update Goal
+//@route    POST /api/goals 
+//@acces    Private
+
+const updateGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Update goal ${req.params.id}` })
+})
+// @desc    delete Goal
+//@route    DELETE /api/goals/:id 
+//@acces    Private
+
+const deleteGoal = asyncHandler(async (req, res) => {
+    res.status(200).json({message: `Delete goal ${req.params.id}`})
+})
 
 module.exports = {
-    getGoals,
+    getGoals, 
+    setGoal, 
+    updateGoal, 
+    deleteGoal,
 }
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,19 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getGoals} = require ('../controllers/goalController')
+const {getGoals, setGoal, updateGoal, deleteGoal} = require ('../controllers/goalController')
 
-router.get('/', getGoals)
+router.route('/').get(getGoals).post(setGoal)
+router.route('/').delete(deleteGoal).put(updateGoal)
 
-router.post('/', (req,res) => {
-    res.status(200).json({message: "Set goals"})
-})
-
-router.put('/:id', (req,res) => {
-    res.status(200).json({message: `Update goal ${req.params.id}` })
-})
-
-router.delete('/:id', (req,res) => {
-    res.status(200).json({message: `Delete goal ${req.params.id}`})
-})
 
 module.exports = router
